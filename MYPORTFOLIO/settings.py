1cdfebc8675de30b02d00c8b36989cd61fdd4de2
sanari2019 <samachoo2010@icloud.com>
2023-04-08 01:19:32 +0100

    additional changes

--- MYPORTFOLIO/settings.py
+++ MYPORTFOLIO/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z&qp^r-u^rdz_c*std&r971#+nqn(pder^qu!2*!gzv&&2op%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
